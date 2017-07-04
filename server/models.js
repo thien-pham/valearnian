5fd41873e4043e668da799fbc10b90a1d09da73e
pham-thien <pham.thien@outlook.com>
2017-07-04 17:28:44 -0400

    added user model

--- server/models.js
+++ server/models.js
@@ -5,7 +5,14 @@ const userSchema = mongoose.Schema({
   accessToken: {type: String}
 });
 
-const User = mongoose.model('User', userSchema)
+const userSchema = mongoose.Schema({
+    googleId: {type: String, required: true},
+    accessToken: {type: String, required: true},
+    name: {type: String, required: false},
+    email: {type: String, required: false},
+    points: {type: Number, required: false}
+})
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = {User};
-
