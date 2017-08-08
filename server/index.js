f451ccccf7187c78c44ae1b7ebbbdf82fe832ab1
pham-thien <pham.thien@outlook.com>
2017-08-08 19:17:56 -0400

    typo

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const { User, Question } = require('./models');
 let secret = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  DATABASE_URL: process.env.SERVER
+  DATABASE_URL: process.env.DATABASE_URL
 };
 
 if(process.env.NODE_ENV != 'production') {
