d730899de09e105e8259a292e31dd2b764bdb6d5
pham-thien <pham.thien@outlook.com>
2017-08-08 17:30:48 -0400

    edited config

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
-exports.DATABASE_URL = 'mongodb://user:123@ds145312.mlab.com:45312/valearnian';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user:123@ds145312.mlab.com:45312/valearnian';
 exports.PORT = process.env.PORT || 3001 || 'https://valearnon.herokuapp.com';
