635864c1ccf9a47dcd9fab12c1e70adbee725f86
pham-thien <pham.thien@outlook.com>
2017-08-08 18:48:04 -0400

    added engines property

--- server/config.js
+++ server/config.js
@@ -1,3 +1,17 @@
 require('dotenv').config();
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user:123@ds145312.mlab.com:45312/valearnian';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
 exports.PORT = process.env.PORT || 3001 || 'https://valearnon.herokuapp.com';
+
+require( 'dotenv' ).config( );
+exports.DATABASE_URL 			= process.env.DATABASE_URL || global.DATABASE_URL;
+exports.TEST_DATABASE_URL = ( process.env.TEST_DATABASE_URL );
+exports.PORT 							= process.env.PORT || 8080;
+exports.TWILIO_SID 				= process.env.TWILIO_SID;
+exports.TWILIO_AUTH 			= process.env.TWILIO_AUTH;
+exports.TWILIO_PHONE 			= process.env.TWILIO_PHONE;
+exports.APP_URL 					= process.env.URL;
+exports.CLIENT_ID         = process.env.CLIENT_ID;
+exports.CLIENT_SECRET     = process.env.CLIENT_SECRET;
+exports.URL 							= process.env.NODE_ENV === 'production'
+	? "https://mighty-depths-52749.herokuapp.com/"
+	: 'http://localhost:8080';
