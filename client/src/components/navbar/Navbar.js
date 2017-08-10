a7591ddc1e31f61dcd3df7d97ff80bec1cea2009
pham-thien <pham.thien@outlook.com>
2017-08-10 01:24:55 -0400

    css

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -24,8 +24,8 @@ export class Navbar extends Component {
         <a href={'#'}>VALEARNIAN</a>
       </div>
         <ul className={'nav navbar-nav'}>
-          <li className={'label label-default'}>Question #{this.props.questionCount}</li>
-          <li className={'label label-default'}>Score: {this.props.score}</li>
+          <li><h3><span className={'label label-default'}>Question # {this.props.questionCount}</span></h3></li>
+          <li><h3><span className={'label label-default score'}>Score: {this.props.score} {points}</span></h3></li>
         </ul>
         <ul className={'nav navbar-nav navbar-right'}>
           <li><a href={'/api/auth/logout'}><span className='glyphicon glyphicon-log-out'>{'Logout'}</span></a></li>
